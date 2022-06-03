8b17312c0b8a2a2b5845ee0e7ed4974d4dbd5934
l0stg <Utmnibas2510>
2022-06-03 21:16:44 +0500

    Query запрос

--- app/src/main/java/com/example/gitrepositoriesapp/ApiService.kt
+++ app/src/main/java/com/example/gitrepositoriesapp/ApiService.kt
@@ -3,8 +3,13 @@ package com.example.gitrepositoriesapp
 import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
+
+
 
 interface ApiService {
+
     @GET("/repositories")
-    fun getData(): Call<MutableList<dataModels>>
+
+    fun getData(@Query("since") sin: Int = 369 ): Call<MutableList<dataModels>>
 }
