57f5652f15611775fde81c27dfea33ce924f3ef5
l0stg <Utmnibas2510>
2022-06-03 12:23:08 +0500

    Retrofit Complit

---
+++ app/src/main/java/com/example/gitrepositoriesapp/ApiService.kt
@@ -0,0 +1,9 @@
+package com.example.gitrepositoriesapp
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface ApiService {
+    @GET("/repositories")
+    fun getDataList(): Call<MutableList<DataModels>>
+}
